1b6a71031b1f201c52efe5379d29cdde6d187e95
AAstrup <alexander.westi.astrup@gmail.com>
2017-10-24 01:19:16 +0200

    Counter updated with event on update funciton

--- Server/Server/ClientProject/Counter.cs
+++ Server/Server/ClientProject/Counter.cs
@@ -3,12 +3,17 @@ using System.Collections.Generic;
 
 public class Counter : IUpdatable
 {
+    private float totalDuration;
     public float duration;
     private List<timesOut> triggersWhenTimesOut;
     public delegate void timesOut();
+    public event timeUpdated timeUpdateEvent;
+    public delegate void timeUpdated(float currentTime,float totalTime);
+
 
     public Counter(float duration, List<timesOut> triggersWhenTimesOut)
     {
+        totalDuration = duration;
         this.duration = duration;
         this.triggersWhenTimesOut = triggersWhenTimesOut;
     }
@@ -21,6 +26,8 @@ public class Counter : IUpdatable
     public void Update(float deltaTime)
     {
         duration -= deltaTime;
+        if(timeUpdateEvent != null)
+            timeUpdateEvent.Invoke(duration,totalDuration);
     }
 
     public void End()
